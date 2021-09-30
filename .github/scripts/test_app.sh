ef5f59cacf68de07d3fd6b645053acb81f3233d9
Bruna Drago <bruna_drago@hotmail.com>
2021-09-30 18:30:58 -0300

    Update test_app.sh

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -4,3 +4,4 @@ xcodebuild -workspace Teste_ML.xcworkspace \
             -scheme Teste_ML  \
             -destination platform=iOS\ Simulator,OS=14.4,name=iPhone\ 11 \
             clean test | xcpretty
+
