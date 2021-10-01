def671bef672a581d3b4ef418b3c953e9eb14a01
Bruna Drago <bruna_drago@hotmail.com>
2021-10-01 13:08:56 -0300

    changing iphone version

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -2,6 +2,6 @@ set -eo pipefail
 
 xcodebuild -workspace Teste_ML.xcworkspace \
             -scheme Teste_ML \
-            -destination platform=iOS\ Simulator,OS=14.4,name=iPhone\ 11 \
+            -destination platform=iOS\ Simulator,OS=14.5,name=iPhone\ 11 \
             clean test | xcpretty
 
