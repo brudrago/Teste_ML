c90e303556f23a2b2480dd2ed611c83d14f7af0b
Bruna Drago <bruna_drago@hotmail.com>
2021-10-01 15:03:27 -0300

    teste versao 13.0

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -2,6 +2,6 @@ set -eo pipefail
 
 xcodebuild -workspace Teste_ML.xcworkspace \
             -scheme Teste_ML \
-            -destination platform=iOS Simulator,name=iPhone 8,OS=13.2.2 \
+            -destination platform=iOS\ Simulator,OS=14.4,name=iPhone\ 11 \
             clean test | xcpretty
 
