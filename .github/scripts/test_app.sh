b4a3a68327c3303d584710c2480c32ee8c5eb4b6
Bruna Drago <bruna_drago@hotmail.com>
2021-10-01 12:49:27 -0300

    changing script

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -1,7 +1,7 @@
 set -eo pipefail
 
 xcodebuild -workspace Teste_ML.xcworkspace \
-            -scheme Teste_ML \ iOS \
+            -scheme Teste_ML \
             -destination platform=iOS\ Simulator,OS=14.4,name=iPhone\ 11 \
             clean test | xcpretty
 
